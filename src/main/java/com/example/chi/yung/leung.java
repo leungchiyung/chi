069171f164bcdb55f20a1fd93e0584cca946913e
leungchiying <leungchiyung1112@gmail.com>
2022-09-24 01:47:20 +0800

    addhaha

--- src/main/java/com/example/chi/yung/leung.java
+++ src/main/java/com/example/chi/yung/leung.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class leung {
     @RequestMapping("/yung")
     public String helloworld() {
-        return "hello world!!!";
+        return "HAHAH hello world!!!";
     }
 }
 
